f89619333f480fe9692535f663491f435e218cad
Jamshidbek <jamanu1910077@gmail.com>
2023-01-24 18:35:44 +0500

    light dark theme changed

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /** @format */
-const toggle = document.getElementById("toggleDark");
+const toggle = document.getElementById("whiteLabel");
 const body = document.querySelector("body");
 
 toggle.addEventListener("click", () => {
-  if (toggle.innerHTML == "dark") {
+  if (toggle.id == "whiteLabel") {
     //Body
     body.style.backgroundColor = dark.backgroundColor;
     body.style.transition = dark.transition;
-    toggle.innerHTML = "light";
+    toggle.id = "darkLabel";
     //***************
   } else {
     //Body
     body.style.backgroundColor = light.backgroundColor;
     body.style.transition = light.transition;
-    toggle.innerHTML = "dark";
+    toggle.id = "whiteLabel";
     //***************
   }
 });
