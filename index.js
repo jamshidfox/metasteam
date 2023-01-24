a06bf8ffdaf51c99fe78b5c6029e53d4c53e7457
Jamshidbek <jamanu1910077@gmail.com>
2023-01-24 22:53:29 +0500

    section handled

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 const toggle = document.getElementById("darkLabel");
 const metaText = document.getElementsByClassName("meta-text");
 const languageChanger = document.getElementsByClassName("animate");
+const leftContainer = document.getElementsByClassName("left-container");
+const H5 = document.getElementById("h5");
+const telegramIcon = document.getElementById("telegram-icon");
+const instagramIcon = document.getElementById("instagram-icon");
+const facebookIcon = document.getElementById("facebook-icon");
 const body = document.querySelector("body");
-console.log(languageChanger);
+
 toggle.addEventListener("click", () => {
   if (toggle.id == "whiteLabel") {
     //Body
@@ -18,6 +23,14 @@ toggle.addEventListener("click", () => {
     languageChanger[1].style.color = dark.navbar.color;
     languageChanger[2].style.color = dark.navbar.color;
     //****************
+
+    //Section
+    leftContainer[0].style.color = dark.section.color;
+    H5.style.color = dark.section.h5color;
+    telegramIcon.style.color = dark.section.iconColors;
+    instagramIcon.style.color = dark.section.iconColors;
+    facebookIcon.style.color = dark.section.iconColors;
+    //****************
   } else {
     //Body
     body.style.backgroundColor = light.backgroundColor;
@@ -30,6 +43,14 @@ toggle.addEventListener("click", () => {
     languageChanger[0].style.color = light.navbar.color;
     languageChanger[1].style.color = light.navbar.color;
     languageChanger[2].style.color = light.navbar.color;
+    //****************
+
+    //Section
+    leftContainer[0].style.color = light.section.color;
+    H5.style.color = light.section.h5color;
+    telegramIcon.style.color = light.section.iconColors;
+    instagramIcon.style.color = light.section.iconColors;
+    facebookIcon.style.color = light.section.iconColors;
     //****************
   }
 });
